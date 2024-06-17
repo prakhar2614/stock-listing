fb0ca0f469bad2117a92308e34fcce83b7916a3e
prakharsaki <prakhar2614@gmail.com>
2024-06-17 23:36:13 +0530

    created header for nav bar

---
+++ components/header.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+.nav 
+	.start 
+		.p HOME       
+		.p SCREENS 
+		.p TOOLS
+		i.mi arrow_drop_down
+	.end 
+		.p Login
+		.p Create Account
+</template>
+
+<style lang="sass">
+.nav
+	display: flex
+	justify-content: space-around
+	font-size: 3rem
+	background: red
+	.start
+		cursor: pointer
+		display: flex
+		justify-content: space-evenly		
+	.end
+		cursor: pointer
+		display: flex
+		justify-content: space-evenly
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,4 @@
+<template lang="pug">
+Header  
+h3 Finvest  
+</template>
